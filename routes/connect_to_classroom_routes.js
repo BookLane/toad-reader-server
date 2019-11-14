4e6ac1294a5d5ae6f39d60fe20756e5e68af5e68
Huberts <huberts@Hubertss-iMac.lan>
2019-11-14 13:01:22 +0200

    Fixed connect to classroom query bug

--- routes/connect_to_classroom_routes.js
+++ routes/connect_to_classroom_routes.js
@@ -2,28 +2,36 @@ const util = require('../util');
 
 module.exports = function (app, connection, ensureAuthenticatedAndCheckIDP, log) {
 
-  app.post('/connect_to_classroom/:accessCode', ensureAuthenticatedAndCheckIDP, function (req, res, next) {
+  app.post('/connect_to_classroom', ensureAuthenticatedAndCheckIDP, function (req, res, next) {
 
+    if(!util.paramsOk(req.body, ['code'])) {
+      log(['Invalid parameter(s)', req.body], 3);
+      res.status(400).send();
+      return;
+    }
+
+    const { code } = req.body
     const now = util.timestampToMySQLDatetime(null, true);
 
-    connection.query(''
-      + 'SELECT c.uid, c.access_code, c.instructor_access_code, bi.version '
-      + 'FROM `classroom` as c '
-      + 'LEFT JOIN `book_instance` as bi ON (bi.id=c.book_id) '
-      + 'WHERE ( '
-        + 'c.access_code=? ',
-        + 'OR c.instructor_access_code=? ',
-      + ') '
-      + 'AND c.idp_id=? ',
-      + 'AND bi.idp_id=? ',
-      + 'AND bi.user_id=? ',
-      + 'AND bi.version IN (?) ',
-      + 'AND (bi.expires_at IS NULL OR bi.expires_at>?) '
-      + 'AND (bi.enhanced_tools_expire_at IS NULL OR bi.enhanced_tools_expire_at>?) '
-      + '',
+    connection.query(`
+      SELECT c.uid, c.access_code, c.instructor_access_code, bi.version
+      FROM classroom as c
+        LEFT JOIN book_instance as bi ON (bi.id=c.book_id)
+      WHERE
+        (
+          c.access_code=? 
+          OR c.instructor_access_code=? 
+        )
+        AND c.idp_id=? 
+        AND bi.idp_id=? 
+        AND bi.user_id=? 
+        AND bi.version IN (?) 
+        AND (bi.expires_at IS NULL OR bi.expires_at>?)
+        AND (bi.enhanced_tools_expire_at IS NULL OR bi.enhanced_tools_expire_at>?)
+      `,
       [
-        req.params.accessCode,
-        req.params.accessCode,
+        code,
+        code,
         req.user.idpId,
         req.user.idpId,
         req.user.id,
@@ -35,7 +43,7 @@ module.exports = function (app, connection, ensureAuthenticatedAndCheckIDP, log)
         if (err) return next(err);
 
         if(rows.length === 0) {
-          log(['Invalid access code or user does not have required book_instance', req.params.accessCode, req.user.id], 3);
+          log(['Invalid access code or user does not have required book_instance', code, req.user.id], 3);
           res.status(400).send();
           return;
         }
@@ -55,10 +63,10 @@ module.exports = function (app, connection, ensureAuthenticatedAndCheckIDP, log)
           (err2, rows2) => {
             if (err2) return next(err2);
 
-            const isInstructorAccessCode = classroom.instructor_access_code === req.params.accessCode;
+            const isInstructorAccessCode = classroom.instructor_access_code === code;
 
             if(isInstructorAccessCode && classroom.version !== 'INSTRUCTOR') {
-              log(['Non-instructor attempting to use instructor access code', req.params.accessCode, req.user.id], 3);
+              log(['Non-instructor attempting to use instructor access code', code, req.user.id], 3);
               res.status(400).send();
               return;
             }
@@ -77,7 +85,7 @@ module.exports = function (app, connection, ensureAuthenticatedAndCheckIDP, log)
             if(classroomMember) {
               if(classroomMember.role === 'INSTRUCTOR' && !isInstructorAccessCode) {
                 // we are not going to degrade them, so do nothing
-                res.status(200).send();
+                res.status(400).send();
                 return;
               } else {
                 insertOrUpdate = 'UPDATE `classroom_member` SET ? WHERE classroom_uid=? AND user_id=?';
@@ -97,7 +105,7 @@ module.exports = function (app, connection, ensureAuthenticatedAndCheckIDP, log)
               ],
               (err, results) => {
                 if (err) return next(err);
-                res.status(200).send();
+                res.status(200).send({ uid: classroom.uid });
               }
             );
           }
@@ -107,13 +115,19 @@ module.exports = function (app, connection, ensureAuthenticatedAndCheckIDP, log)
 
   });
 
-  app.post('/leave_classroom/:classroomUid', ensureAuthenticatedAndCheckIDP, function (req, res, next) {
+  app.post('/leave_classroom', ensureAuthenticatedAndCheckIDP, function (req, res, next) {
+
+    if(!util.paramsOk(req.body, ['classroomUid'])) {
+      log(['Invalid parameter(s)', req.body], 3);
+      res.status(400).send();
+      return;
+    }
 
     connection.query(
       'UPDATE `classroom_member` SET ? WHERE classroom_uid=? AND user_id=?',
       [
         { deleted_at: null },
-        req.params.classroomUid,
+        req.body.classroomUid,
         req.user.id,
       ],
       (err, results) => {
