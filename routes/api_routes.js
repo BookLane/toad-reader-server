712b4155444ceacee071b3b6079744bc72847e20
Huberts <huberts@Hubertss-iMac.lan>
2020-01-23 09:43:55 +0200

    prepped /updateuserinfo (not active yet, just logging)

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,48 +1,52 @@
-var util = require('../util');
-const jwt = require('jsonwebtoken');
-
-const decodeJWT = (req, res, next) => {
-
-  connection.query(
-    'SELECT id, userInfoJWT FROM `idp` WHERE domain=?',
-    [util.getIDPDomain(req.headers.host)],
-    (err, rows) => {
-      if (err) return next(err);
-
-      if(rows.length !== 1) {
-        log(["API request from invalid host.", req.headers.host], 3);
-        return res.status(403).send({ success: false });
-      }
-
-      try {
-        req.params.payload_decoded = jwt.verify(req.params.payload, rows[0].userInfoJWT);
-        req.idpId = parseInt(rows[0].idp, 10);
-      } catch(err) {
-        log(["Invalid API payload.", req.headers.host, err], 3);
-        return res.status(403).send({ success: false });
-      }
-
-      return next();
-    },
-  )
-
-}
+var util = require('../util')
+const jwt = require('jsonwebtoken')
 
 module.exports = function (app, connection, log) {
 
-  app.get('/updateuserinfo',
+  const decodeJWT = async (req, res, next) => {
+
+    const rows = await util.runQuery({
+      query: `SELECT id, userInfoJWT FROM idp WHERE domain=:domain`,
+      vars: {
+        domain: util.getIDPDomain(req.headers.host),
+      },
+      connection,
+      next,
+    })
+
+    if(rows.length !== 1) {
+      log(["API request from invalid host.", req.headers.host], 3)
+      return res.status(403).send({ success: false })
+    }
+
+    try {
+      req.body.payload_decoded = jwt.verify(req.body.payload, rows[0].userInfoJWT)
+      req.idpId = parseInt(rows[0].idp, 10)
+    } catch(err) {
+      log(["Invalid API payload.", req.headers.host, err], 3)
+      return res.status(403).send({ success: false })
+    }
+
+    return next()
+
+  }
+
+  app.post('/updateuserinfo',
     decodeJWT,
     (req, res, next) => {
-      util.updateUserInfo({
-        connection,
-        log,
-        userInfo: req.params.payload_decoded,
-        idpId: req.idpId,
-        next,
-      }).then(() => {
-        res.send({ success: true });
-      });
+      log(["/updateuserinfo post", req.body], 2)
+      res.send({ success: true })
+
+      // util.updateUserInfo({
+      //   connection,
+      //   log,
+      //   userInfo: req.body.payload_decoded,
+      //   idpId: req.idpId,
+      //   next,
+      // }).then(() => {
+      //   res.send({ success: true })
+      // })
     }
-  );
+  )
 
 }
