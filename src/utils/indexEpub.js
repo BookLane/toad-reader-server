065592dbc3ab222776af7446ae77f951dd3dbdf7
x <x@xxxxxxx.dev>
2024-04-13 00:01:24 +0000

    Quadrupling memory limit for EPUB indexing

--- src/utils/indexEpub.js
+++ src/utils/indexEpub.js
@@ -66,10 +66,10 @@ const getIndexedBook = async ({ baseUri, spines, log }) => {
 
     const memoryUsageInMB = parseInt(process.memoryUsage().rss/1000000)
 
-    if(memoryUsageInMB > 500) {
+    if(memoryUsageInMB > 2000) {
       throw new Error(`EPUB search index overloading memory (~${memoryUsageInMB} mb)`)
 
-    } else if(global.gc && spineIndex % 10 === 0 && memoryUsageInMB > 250) {
+    } else if(global.gc && spineIndex % 10 === 0 && memoryUsageInMB > 1000) {
       try {
         process.stdout.clearLine()
         process.stdout.cursorTo(0)
